db42514646403dd697767129d8468026117f5c43
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-13 15:43:32 +0900

    added GET purchases endpoint

--- api/index.py
+++ api/index.py
@@ -8,5 +8,10 @@ def index():
     return "<h1>Hello, Flask!</h1>"
 
 
+@app.route("/purchases")
+def get_purchases():
+    return ""
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
