e4313df72d4dfc78171ba5e8d96be9ab933544c5
YOKOOLAB-PC\youbo <y85262860@gmail.com>
2021-09-14 10:17:49 +0900

    q

--- front/src/pages/InfluencerPersonal.js
+++ front/src/pages/InfluencerPersonal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// Please change as needed
+
 function InfluencerPersonal() {
   return (
-    <h1>This is influencerPersonal page</h1>
+    <div>This is influencerPersonal page</div>
   );
 }
 
