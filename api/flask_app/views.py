f4ab93c4ed316dddb4c075bafcfcf51291d0eedd
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-14 13:48:08 +0900

    added base64 convert function and put user endpoint

--- api/flask_app/views.py
+++ api/flask_app/views.py
@@ -1,3 +1,4 @@
+import base64
 from flask_app import app, db
 from flask import jsonify, request
 from .models.purchases import Purchase
@@ -36,7 +37,34 @@ def get_user(id):
     return jsonify(user)
 
 
+@app.route("/users/<int:id>", methods=['PUT'])
+def put_user(id):
+    user = db.session.query(User).get(id)
+    if user is None:
+        return jsonify({'message': 'the user was not found'}), 404
+    payload = request.json
+    user.nickname = payload.get('nickname')
+    user.youtube_url = payload.get('youtube')
+    user.twitter_screenname = payload.get('twitter_screenname')
+    user.description = payload.get('desc')
+    # save user icon
+    icon = payload.get('img')
+    if icon is not None:
+        src = convert_and_save(icon)
+        user.icon = src
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({}), 200
+
+
 @app.route("/products/<int:id>", methods=['GET'])
 def get_product(id):
     product = db.session.query(Product).get(id)
     return jsonify(product)
+
+
+def convert_and_save(b64_string):
+    FILE_NAME = "imageToSave.png"
+    with open(FILE_NAME, "wb") as fh:
+        fh.write(base64.decodebytes(b64_string.encode()))
+    return FILE_NAME
