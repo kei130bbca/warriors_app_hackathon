ba1f48c94fc6bfb67ada50b4680d69d31e2a6807
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-14 10:20:45 +0900

    added DB settings

--- api/flask_app/__init__.py
+++ api/flask_app/__init__.py
@@ -5,7 +5,10 @@ from flask import Flask
 from flask import Flask
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:docker@db/test_db?charset=utf8"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['DEBUG'] = True
+
 db = SQLAlchemy()
 db.init_app(app)
 migrate = Migrate(app, db)
