04f9907ed4c81b0fc284c6f601a613f19c56f307
YOKOOLAB-PC\youbo <y85262860@gmail.com>
2021-09-14 10:03:17 +0900

    create influencerPersonal page

---
+++ front/src/pages/InfluencerPersonal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Please change as needed
+function influencerPersonal() {
+  return (
+    <h1>This is influencerPersonal page</h1>
+  );
+}
+
+export default influencerPersonal;
