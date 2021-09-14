24def247dd6d880476de0a110303ae2e92ec39e6
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-14 21:20:25 +0900

    Initialize the review management page.

---
+++ front/src/pages/ReviewManagementPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function Main() {
+    return (
+        <div>
+            aa
+        </div>
+    );
+}
+
+function ReviewManagementPage() {
+    return (
+        <div>
+            <Header title="Review Management" />
+            <Main />
+            <Footer />
+        </div>
+        
+    );
+}
+
+export default ReviewManagementPage;
