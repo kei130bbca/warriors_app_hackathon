0e2385055ffcc0dd548b2d4fd7d235d1d0f5d5a1
YOKOOLAB-PC\youbo <y85262860@gmail.com>
2021-09-15 09:07:52 +0900

    add influencer's react router

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Route path="/template-page">
       <TemplatePage />
     </Route>
-    <Route path="/influencerpersonal" component = {InfluencerPersonal} >
+    <Route path="/influencerpersonal">
       <InfluencerPersonal />
     </Route>
     </BrowserRouter>
