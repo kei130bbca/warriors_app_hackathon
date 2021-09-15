fc45930090ea6ae9cf3322e73f70ac0b24e8161e
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-15 17:59:46 +0900

    Add CSS style

--- front/src/pages/components/Header.js
+++ front/src/pages/components/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 function Header(props) {
     return (
         <header>
-            <div className="hero is-primary is-bold">
+            {/* <div className="hero is-primary is-bold">
                 <div className="hero-body">
                     <div className="container">
                         <h1>{props.title}</h1>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href={props.path}>
+                    {props.title}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/influencerpersonal">Influencer Personal Page</Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     );
 }
