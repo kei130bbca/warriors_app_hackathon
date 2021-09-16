9e30fab6387e134d3003ed3a6250a6101a0310c4
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-16 13:30:55 +0900

    FIxed auth_user endpoint and Added ProfileEdit page

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import TemplatePage from './pages/TemplatePage';
 import InfluencerPersonal from './pages/InfluencerPersonal';
 import ReviewManagementPage from './pages/ReviewManagementPage';
 import ReviewPostingPage from './pages/ReviewPostingPage';
+import ProfileEdit from './pages/ProfileEdit';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -24,6 +25,9 @@ function App() {
       </Route>
       <Route path="/review-posting/:id">
         <ReviewPostingPage />
+      </Route>
+      <Route path="/profile-edit">
+        <ProfileEdit />
       </Route>
     </BrowserRouter>
   );
