7e240d589473799224ae69a70663272865cca7da
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-16 11:27:18 +0900

    Added key attribute to supress error

--- front/src/pages/components/Header.js
+++ front/src/pages/components/Header.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header(props) {
-    const NavLinkArray = [<Nav.Link href="/">Home</Nav.Link>];
-    if (props.auth === true) {
-        NavLinkArray.push(<Nav.Link href="/influencerpersonal">Influencer Personal Page</Nav.Link>);
-    }
-    return (
-        <header>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href={props.path}>
-                    {/* {props.title} */}
-                    Warriors app
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    {NavLinkArray}
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
+  const NavLinkArray = [
+    <Nav.Link href="/" key="home">
+      Home
+    </Nav.Link>,
+  ];
+  if (props.auth === true) {
+    NavLinkArray.push(
+      <Nav.Link href="/influencerpersonal" key="influencerpersonal">
+        Influencer Personal Page
+      </Nav.Link>
     );
+  }
+  return (
+    <header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href={props.path}>
+          {/* {props.title} */}
+          Warriors app
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {NavLinkArray}
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
