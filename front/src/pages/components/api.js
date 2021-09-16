4593f360126112e4690013f6480e0213b00c049a
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-16 11:25:48 +0900

    fix postLogin function

--- front/src/pages/components/api.js
+++ front/src/pages/components/api.js
@@ -33,6 +33,9 @@ export async function postLogin(username, password) {
     };
     const response = await fetch(`http://localhost:8000/login`, {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;'
+        },
         body: JSON.stringify(request)
     });
     const data = await response.json();
