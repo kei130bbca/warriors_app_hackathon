97d7be0176ca4bc6ad3f7a0552f950895f864705
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-16 20:25:57 +0900

    fix token

--- front/src/pages/components/Header.js
+++ front/src/pages/components/Header.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 function Header(props) {
-  const NavLinkArray = [
-    <Nav.Link href="/" key="home">
-      Home
-    </Nav.Link>,
-  ];
-  if (localStorage.getItem('token')) {
-    NavLinkArray.push(
-      <Nav.Link href="/influencerpersonal" key="influencerpersonal">
-        Influencer Personal Page
-      </Nav.Link>
+    const NavLinkArray = [
+        <Nav.Link href="/" key="home">
+            Home
+        </Nav.Link>,
+    ];
+    const token = localStorage.getItem('token');
+    if (token) {
+        const user_id = localStorage.getItem('user_id');
+        const url = `/influencerpersonal/?user_id=${user_id}`
+        NavLinkArray.push(
+            <Nav.Link href={url} key="home">
+                Personal page
+            </Nav.Link>);
+    }
+    return (
+        <header>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href={props.path}>
+                    {/* {props.title} */}
+                    Warriors app
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    {NavLinkArray}
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
     );
-  }
-  return (
-    <header>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href={props.path}>
-          {/* {props.title} */}
-          Warriors app
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          {NavLinkArray}
-        </Navbar.Collapse>
-      </Navbar>
-    </header>
-  );
 }
 
 export default Header;
