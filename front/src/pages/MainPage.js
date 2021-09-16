6f1f12c9ecaa5c9dd6edfb2a3202920ee300c415
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-16 11:41:26 +0900

    Fixed no key error

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { fetchUser, fetchUsers } from './components/api';
-import { Form, Button, Row, Col, Card, Image, Container, Toast, ToastContainer} from 'react-bootstrap';
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  Card,
+  Image,
+  Container,
+  Toast,
+  ToastContainer,
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReviewCard2 from './components/ReviewCard2';
 
 class User {
-  constructor(id,
+  constructor(
+    id,
     username,
     nickname,
     twitter_screenname,
     youtube_url,
     password,
     icon,
-    desc) {
+    desc
+  ) {
     this.id = id;
     this.username = username;
     this.nickname = nickname;
@@ -27,14 +39,16 @@ class User {
 }
 
 class Purchase {
-  constructor(purchase_id,
+  constructor(
+    purchase_id,
     user_id,
     product_id,
     count,
     bouth_at,
     comment,
     stars,
-    title) {
+    title
+  ) {
     this.purchase_id = purchase_id;
     this.user_id = user_id;
     this.product_id = product_id;
@@ -58,12 +72,12 @@ class Product {
 
 const styles = {
   inlineDisplay: {
-    display: 'inline-block'
+    display: 'inline-block',
   },
   horizontalList: {
     overflowX: 'auto',
     whiteSpace: 'nowrap',
-    webkitOverflowScrolling: 'touch'
+    webkitOverflowScrolling: 'touch',
   },
   item: {
     display: 'inline-block',
@@ -71,16 +85,21 @@ const styles = {
     height: '400px',
     margin: '16px',
     fontSize: '48px',
-    background: 'rgba(255, 0, 0, 0.4)'
-  }
+    background: 'rgba(255, 0, 0, 0.4)',
+  },
 };
-
 
 function Icon(props) {
   return (
     <div>
       <a href="/">
-        <Image rounded src={props.src} width={props.width} height={props.height} alt="" />
+        <Image
+          rounded
+          src={props.src}
+          width={props.width}
+          height={props.height}
+          alt=""
+        />
       </a>
     </div>
   );
@@ -93,9 +112,7 @@ function InfluencerName(props) {
         <Icon src={props.icon} height="100" />
       </div>
       <div style={styles.inlineDisplay}>
-        <h3>
-          {`${props.name}'s recommendations`}
-        </h3>
+        <h3>{`${props.name}'s recommendations`}</h3>
         {props.desc}
       </div>
     </div>
@@ -121,8 +138,6 @@ function Star(props) {
   );
 }
 
-
-
 function ReviewCard(props) {
   const purchase = props.purchase;
   const product = props.product;
@@ -134,14 +149,14 @@ function ReviewCard(props) {
         <p>{purchase.comment}</p>
         <Star stars={purchase.stars} /> */}
       <Card style={{ width: '18rem' }} className="bg-light text-dark">
-        <a href="/" >
+        <a href="/">
           <Card.Img variant="top" src={product.img} />
         </a>
         <Card.Body>
           <Card.Title>{purchase.title}</Card.Title>
           <Card.Text>
             {purchase.comment}
-          <Star stars={purchase.stars} />
+            <Star stars={purchase.stars} />
           </Card.Text>
         </Card.Body>
       </Card>
@@ -164,44 +179,44 @@ function LoginSystem(props) {
   return (
     <div className="container">
       <Container>
-      <Toast>
-        <Toast.Header>
-          <strong className="me-auto">You wanna be a influencer? </strong>
-        </Toast.Header>
-        <Toast.Body>
-        <a href="/influencer-registration-form"> Get registerd!</a>
-        </Toast.Body>
-      </Toast>
-      <Form >
-        {/* <Row className="align-itme-center">
+        <Toast>
+          <Toast.Header>
+            <strong className="me-auto">You wanna be a influencer? </strong>
+          </Toast.Header>
+          <Toast.Body>
+            <a href="/influencer-registration-form"> Get registerd!</a>
+          </Toast.Body>
+        </Toast>
+        <Form>
+          {/* <Row className="align-itme-center">
           <Col xs="auto">
           <h2>You wanna be a influencer? &rarr;  
           <a href="/influencer-registration-form"> get registerd!</a>
           </h2>
           </Col>
         </Row> */}
-        <Row className="align-itme-center">
-          <Col xs>
-            <Form.Group>
-              {/* <Form.Label>username: </Form.Label> */}
-              {/* <input type="text"></input> */}
-              <Form.Control type="email" placeholder="username" />
-            </Form.Group>
-          </Col>
-          <Col xs>
-            <Form.Group>
-              {/* <Form.Label>password: </Form.Label> */}
-              {/* <input type="text"></input> */}
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </Col>
-          <Col xs="auto">
-            <Button variant="primary" type="submit">
-              Log in
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+          <Row className="align-itme-center">
+            <Col xs>
+              <Form.Group>
+                {/* <Form.Label>username: </Form.Label> */}
+                {/* <input type="text"></input> */}
+                <Form.Control type="email" placeholder="username" />
+              </Form.Group>
+            </Col>
+            <Col xs>
+              <Form.Group>
+                {/* <Form.Label>password: </Form.Label> */}
+                {/* <input type="text"></input> */}
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Col>
+            <Col xs="auto">
+              <Button variant="primary" type="submit">
+                Log in
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     </div>
   );
@@ -211,13 +226,14 @@ function Main() {
   // const [users, setUsers] = useState(null);
 
   useEffect(() => {
-    fetchUser(1).then((u) => {
-      // setUsers(u);
-      console.log(u);
-    })
+    fetchUser(1)
+      .then((u) => {
+        // setUsers(u);
+        console.log(u);
+      })
       .catch((e) => {
         console.log(e);
-      })
+      });
   }, []);
 
   const user1 = new User(
@@ -228,7 +244,8 @@ function Main() {
     'https://www.youtube.com/user/HikakinTV',
     'aaabbb',
     'https://images.dog.ceo/breeds/shiba/shiba-8.jpg',
-    'hi, im hikakin.');
+    'hi, im hikakin.'
+  );
   const user2 = new User(
     2,
     'hikakin@gmail',
@@ -237,52 +254,108 @@ function Main() {
     'https://www.youtube.com/user/HikakinTV',
     'aaabbb',
     'https://images.dog.ceo/breeds/shiba/shiba-11.jpg',
-    'hi, im hikakin.')
-  const purchase1 = new Purchase(1, 1, 11, 5, 2020 - 1 - 1, 'This is my best book', 3, 'The greatest book');
-  const purchase2 = new Purchase(2, 2, 12, 2, 2020 - 1 - 1, 'nice apple', 4, 'How nice apple!');
-  const purchase3 = new Purchase(3, 2, 13, 10, 2020 - 1 - 1, 'nice orange', 5, "I'v never seen like this orange!");
-  const purchase4 = new Purchase(3, 2, 13, 10, 2020 - 1 - 1, 'nice orange', 5, "I'v never seen like this orange!");
-  const product1 = new Product(11, 'readable code', 'https://images-na.ssl-images-amazon.com/images/I/51MgH8Jmr3L.jpg', 2200, 'https://books.rakuten.co.jp/rb/11753651/');
-  const product2 = new Product(12, 'apple', 'https://tshop.r10s.jp/ultra-taste/cabinet/daiiti/daiiti_143.jpg?fitin=275:275', 100, 'https://search.rakuten.co.jp/search/mall/%E3%82%8A%E3%82%93%E3%81%94/');
-  const product3 = new Product(13, 'orange', 'https://tshop.r10s.jp/benikou/cabinet/orenge/imgrc0084994537.jpg?fitin=275:275', 200, 'https://item.rakuten.co.jp/benikou/10000723/?iasid=07rpp_10095___et-ktjubpdf-zqv-23e8ef5b-f7a1-4739-8075-3f19a787f7c7');
+    'hi, im hikakin.'
+  );
+  const purchase1 = new Purchase(
+    1,
+    1,
+    11,
+    5,
+    2020 - 1 - 1,
+    'This is my best book',
+    3,
+    'The greatest book'
+  );
+  const purchase2 = new Purchase(
+    2,
+    2,
+    11,
+    2,
+    2020 - 1 - 1,
+    'nice apple',
+    4,
+    'How nice apple!'
+  );
+  const purchase3 = new Purchase(
+    3,
+    2,
+    12,
+    10,
+    2020 - 1 - 1,
+    'nice orange',
+    5,
+    "I'v never seen like this orange!"
+  );
+  const purchase4 = new Purchase(
+    3,
+    2,
+    13,
+    10,
+    2020 - 1 - 1,
+    'nice orange',
+    5,
+    "I'v never seen like this orange!"
+  );
+  const product1 = new Product(
+    11,
+    'readable code',
+    'https://images-na.ssl-images-amazon.com/images/I/51MgH8Jmr3L.jpg',
+    2200,
+    'https://books.rakuten.co.jp/rb/11753651/'
+  );
+  const product2 = new Product(
+    12,
+    'apple',
+    'https://tshop.r10s.jp/ultra-taste/cabinet/daiiti/daiiti_143.jpg?fitin=275:275',
+    100,
+    'https://search.rakuten.co.jp/search/mall/%E3%82%8A%E3%82%93%E3%81%94/'
+  );
+  const product3 = new Product(
+    13,
+    'orange',
+    'https://tshop.r10s.jp/benikou/cabinet/orenge/imgrc0084994537.jpg?fitin=275:275',
+    200,
+    'https://item.rakuten.co.jp/benikou/10000723/?iasid=07rpp_10095___et-ktjubpdf-zqv-23e8ef5b-f7a1-4739-8075-3f19a787f7c7'
+  );
 
   const users = [user1, user2];
   const purchases = [purchase1, purchase2, purchase3, purchase4];
   const products = [product1, product2, product3];
 
+  function getProduct(id) {
+    const product = products.filter((product) => product.id === id);
+    return product.length > 0 ? product[0] : null;
+  }
+
   return (
     <div>
-      {
-        users.map((user) => {
-          let ReviewElementArray = [];
-          return (
-            <div>
-              <InfluencerName name={user.nickname} icon={user.icon} desc={user.desc} />
-              {purchases.map((purchase) => {
-                if (purchase.user_id === user.id) {
-                  products.map((product) => {
-                    if (purchase.product_id === product.id) {
-                      ReviewElementArray.push(
-                        <div className="col">
-                          {/* <ReviewCard purchase={purchase} product={product} /> */}
-                          <ReviewCard2 purchase={purchase} product={product}/>
-                        </div>
-                      );
-                    }
-                  })
-                }
-              })}
-              <div className="container-fluid">
-                <div className="row flex-row row flex-nowrap overflow-auto">
-                  {ReviewElementArray}
-                </div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <InfluencerName
+              name={user.nickname}
+              icon={user.icon}
+              desc={user.desc}
+            />
+            <div className="container-fluid">
+              <div className="row flex-row row flex-nowrap overflow-auto">
+                {purchases
+                  .filter((purchase) => purchase.user_id === user.id)
+                  .map((purchase) => {
+                    return (
+                      <ReviewCard2
+                        purchase={purchase}
+                        product={getProduct(purchase.product_id)}
+                        key={purchase.user_id + '' + purchase.product_id}
+                      />
+                    );
+                  })}
               </div>
-              <br>
-              </br>
             </div>
-          );
-        })
-      }
+            <br></br>
+          </div>
+        );
+      })}
     </div>
   );
 }
