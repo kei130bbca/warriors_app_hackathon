bb63f812e97c1b1935ba00c1a5b57e52c4016979
Wataru Okamoto <okamoto.kawalab@gmail.com>
2021-09-17 10:15:54 +0900

    fix to show user's reviews

--- front/src/pages/ReviewManagementPage.js
+++ front/src/pages/ReviewManagementPage.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Button, Container, Col } from 'react-bootstrap';
 import Purchase from '../classes/Purchase';
 import ReviewCard2 from './components/ReviewCard2';
+import { fetchPurchases } from './components/api';
+import { useHistory } from 'react-router-dom';
 
 function ReviewManagementPage() {
-  const purchase2 = new Purchase(
-    2,
-    2,
-    12,
-    2,
-    2020 - 1 - 1,
-    'nice apple',
-    4,
-    'How nice apple!'
-  );
-  const purchase3 = new Purchase(
-    3,
-    2,
-    13,
-    10,
-    2020 - 1 - 1,
-    'nice orange',
-    5,
-    "I'v never seen like this orange!"
-  );
+  const [purchases, setPurchases] = useState([]);
+  const [userId, setUserId] = useState(null);
+  const history = useHistory();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetch('http://localhost:8000/auth_user', {
+        headers: {
+          'Content-Type': 'application/json;',
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((res) => {
+        if (res.ok === true) {
+          return res.json();
+        } else { throw ('token is expired.') }
+      }).then((data) => {
+        // console.log(data);
+          localStorage.setItem('user_id', data.id);
+          setUserId(data.id);
+          console.log(data.id);
+        
+      })
+        .catch((e) => {
+          console.log(e);
+          localStorage.removeItem('token');
+          localStorage.removeItem('user_id');
+          history.push({ pathname: "/"});
+        })
+    }
+  }, []);
 
-  const purchases = [purchase2, purchase3];
+  useEffect(() => {
+    fetchPurchases(userId)
+    .then((data) => {
+      console.log(data);
+      setPurchases(data);
+    })
+  }, [userId]);
 
   return (
     <Container className="text-center">
@@ -36,14 +54,14 @@ function ReviewManagementPage() {
         ))}
       </Row>
       <Col className="my-5">
-        <Button
+        {/* <Button
           size="lg"
           onClick={() => {
-            // history.push(`/influencerpersonal/?user_id=${data.id}`);
+            // history.push(`/influencerpersonal/?user_id=${userId}`);
           }}
         >
           Add new review
-        </Button>
+        </Button> */}
       </Col>
     </Container>
   );
